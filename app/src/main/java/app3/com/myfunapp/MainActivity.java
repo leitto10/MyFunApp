771e9acb2e11a4e13feb27f7fe5118f1446d5ae2
Jose Lopez <leitto10@gmail.com>
2019-09-23 23:17:36 -0700

    validating connectivity

--- app/src/main/java/app3/com/myfunapp/MainActivity.java
+++ app/src/main/java/app3/com/myfunapp/MainActivity.java
@@ -1,8 +1,12 @@
 package app3.com.myfunapp;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -30,34 +34,53 @@ public class MainActivity extends AppCompatActivity {
         String forcastURL = "https://api.darksky.net/forecast/" +
                 apiKey + "/" + latitud + "," + longitud;
 
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder()
-                                    .url(forcastURL)
-                                    .build();
-        Call call = client.newCall(request);
-        call.enqueue(new Callback() {
-            @Override
-            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+        if(isNetworkAvailable()) {
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(forcastURL)
+                    .build();
+            Call call = client.newCall(request);
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(@NotNull Call call, @NotNull IOException e) {
 
-            }
-
-            @Override
-            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
-                try {
-                    Log.e(TAG, response.body().string());
-                    if(response.isSuccessful()){
-
-                    }else{
-                        alertUserAboutErroe();
-                    }
-                } catch (IOException e) {
-                    Log.e(TAG, "ID exception caught: ", e);
                 }
-            }
-        });
+
+                @Override
+                public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                    try {
+                        Log.e(TAG, response.body().string());
+                        if (response.isSuccessful()) {
+
+                        } else {
+                            alertUserAboutErroe();
+                        }
+                    } catch (IOException e) {
+                        Log.e(TAG, "ID exception caught: ", e);
+                    }
+                }
+            });
+        }
         Log.d(TAG, "Main UI is running....");
     }
 
+    private boolean isNetworkAvailable() {
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+
+        boolean isAvailable = false;
+
+        if(networkInfo != null && networkInfo.isConnected()){
+            isAvailable = true;
+        }else{
+            Toast.makeText(this, "Sorry, the network is unavailable.", Toast.LENGTH_LONG).show();
+
+        }
+        return isAvailable;
+    }
+
     private void alertUserAboutErroe() {
+        AlertDialogeFragment dialog = new AlertDialogeFragment();
+        dialog.show(getFragmentManager(), "error_dialog");
     }
 }
